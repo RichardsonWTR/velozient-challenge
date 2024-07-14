d9cd559e7598341aa071637c60a034c468cd2fea
Richardson Rosa <richardsonwtr@gmail.com>
2024-07-14 14:25:04 -0300

    feat: adding the project for the twisted fizz buzz

---
+++ EasyChallenge/TwistedFizzBuzzProgram/Program.cs
@@ -0,0 +1,27 @@
+﻿using FizzBuzzSolver;
+
+
+Console.WriteLine("1. Solving values from -20 to 127 using the following rules:");
+Console.WriteLine("-- For multiples of 5 add to the return \"Fizz\"");
+Console.WriteLine("-- For multiples of 9 add to the return \"Buzz\"");
+Console.WriteLine("-- For multiples of 27 add to the return \"Bar\"");
+
+var tokensGenerator = new GenerateAllFizzBuzzTokens(-20, 127);
+
+var customRules = new Dictionary<int, string> {
+    { 5, "Fizz" },
+    { 9, "Buzz" },
+    { 27, "Bar" },
+};
+tokensGenerator.SetTokenPairs(customRules);
+
+printAllTokens(tokensGenerator.GetAllTokens());
+
+
+static void printAllTokens(List<string> tokens)
+{
+    foreach (var token in tokens)
+    {
+        Console.WriteLine(token);
+    }
+}
