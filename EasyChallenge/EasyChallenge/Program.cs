dad857141350a1026a5e72177636e7921bde51a7
Richardson Rosa <richardsonwtr@gmail.com>
2024-07-14 10:54:28 -0300

    feat: adding the original solution for the fizz buzz problem

---
+++ EasyChallenge/EasyChallenge/Program.cs
@@ -0,0 +1,13 @@
+﻿for (int i = 1; i <= 100; i++)
+{
+    var token = "";
+    if (i % 3 == 0)
+        token += "Fizz";
+    if (i % 5 == 0)
+        token += "Buzz";
+
+    if (token.Length == 0)
+        Console.WriteLine(i);
+    else 
+        Console.WriteLine(token);
+}
