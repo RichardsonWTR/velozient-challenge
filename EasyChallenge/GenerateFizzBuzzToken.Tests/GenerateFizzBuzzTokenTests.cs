ad8901923e9d5e9dbedc29c56a7ae5920f81bf4c
Richardson Rosa <richardsonwtr@gmail.com>
2024-07-14 12:01:12 -0300

    tests: adding the first tests of the fizz buzz solver

--- EasyChallenge/GenerateFizzBuzzToken.Tests/GenerateFizzBuzzTokenTests.cs
+++ EasyChallenge/GenerateFizzBuzzToken.Tests/GenerateFizzBuzzTokenTests.cs
@@ -1,10 +1,41 @@
-namespace GenerateFizzBuzzToken.Tests;
+using EasyChallenge;
+
+namespace FizzBuzzTokenTests;
 
 public class GenerateFizzBuzzTokenTests
 {
     [Fact]
     public void GetToken_Input3_ReturnsFizz()
     {
-        // TODO
+        var actual = new GenerateFizzBuzzToken().GetToken(3);
+        Assert.Equal("Fizz", actual);
+    }
+
+    [Fact]
+    public void GetToken_InputMultipleOf3_ReturnsFizz()
+    {
+        var actual = new GenerateFizzBuzzToken().GetToken(6);
+        Assert.Equal("Fizz", actual);
+    }
+
+    [Fact]
+    public void GetToken_Input5_ReturnsBuzz()
+    {
+        var actual = new GenerateFizzBuzzToken().GetToken(5);
+        Assert.Equal("Buzz", actual);
+    }
+
+    [Fact]
+    public void GetToken_InputMultipleOf5_ReturnsBuzz()
+    {
+        var actual = new GenerateFizzBuzzToken().GetToken(10);
+        Assert.Equal("Buzz", actual);
+    }
+
+    [Fact]
+    public void GetToken_InputMultipleOf3And5_ReturnsFizzBuzz()
+    {
+        var actual = new GenerateFizzBuzzToken().GetToken(15);
+        Assert.Equal("FizzBuzz", actual);
     }
 }
