d1b15f38d69e7934529f48e592cd89aa0b149777
Richardson Rosa <richardsonwtr@gmail.com>
2024-07-14 13:57:32 -0300

    feat: new module to generate a range of numbers

---
+++ EasyChallenge/FizzBuzzSolver/GenerateRange.cs
@@ -0,0 +1,13 @@
+﻿namespace FizzBuzzSolver;
+
+public static class GenerateRange
+{
+    public static int[] GenerateRangeFromNumbers(int start, int end)
+    {
+        List<int> result = new List<int>();
+        for (int i = start; i <= end; i++)
+            result.Add(i);  
+
+        return result.ToArray();
+    }
+}
---
+++ EasyChallenge/GenerateFizzBuzzToken.Tests/GenerateRangeTests.cs
@@ -0,0 +1,23 @@
+﻿namespace FizzBuzzSolver.Tests;
+
+public class GenerateRangeTests
+{
+    [Theory]
+    [InlineData(1, 5, new[] {1, 2, 3, 4, 5})]
+    [InlineData(3, 6, new[] {3, 4, 5, 6})]
+
+    public void GenerateRangeFromNumbers_InputPositiveNumbers_GenerateTheCorrectValues(int start, int end, int[] expected)
+    {
+        var actual = GenerateRange.GenerateRangeFromNumbers(start, end);
+        Assert.Equal(expected, actual);
+    }
+
+    [Theory]
+    [InlineData(-1, 1, new[] { -1, 0, 1})]
+    [InlineData(-6, -3, new[] { -6, -5, -4, -3 })]
+    public void GenerateRangeFromNumbers_InputWithNegativeNumbers_GenerateTheCorrectValues(int start, int end, int[] expected)
+    {
+        var actual = GenerateRange.GenerateRangeFromNumbers(start, end);
+        Assert.Equal(expected, actual);
+    }
+}
