6e406df9bab119db07bbf4382ce818c24cdca6a7
Richardson Rosa <richardsonwtr@gmail.com>
2024-07-14 12:56:23 -0300

    feat: generate all tokens can receive custom sequences

--- EasyChallenge/FizzBuzzSolver/GenerateAllFizzBuzzTokens.cs
+++ EasyChallenge/FizzBuzzSolver/GenerateAllFizzBuzzTokens.cs
@@ -5,12 +5,27 @@ namespace FizzBuzzSolver;
 
 public class GenerateAllFizzBuzzTokens
 {
+    private readonly int _startNumber;
+    private readonly int _finalNumber;
+
+    public GenerateAllFizzBuzzTokens()
+    {
+        _startNumber = 1;
+        _finalNumber = 100;
+    }
+
+    public GenerateAllFizzBuzzTokens(int startNumber, int finalNumber)
+    {
+        _startNumber = startNumber;
+        _finalNumber = finalNumber;
+    }
+
     public List<string> GetAllTokens()
     {
         var allTokens = new List<string>();
         var tokenGenerator = new GenerateFizzBuzzToken();
 
-        for (int i = 1; i <= 100; i++)
+        for (int i = _startNumber; i <= _finalNumber; i++)
         {
             var tokenOfNumber = tokenGenerator.GetToken(i);
             if (tokenOfNumber == null)
--- EasyChallenge/GenerateFizzBuzzToken.Tests/GenerateAllFizzBuzzTokensTests.cs
+++ EasyChallenge/GenerateFizzBuzzToken.Tests/GenerateAllFizzBuzzTokensTests.cs
@@ -23,5 +23,16 @@ public class GenerateAllFizzBuzzTokensTests
         Assert.Equal(expectedValue, actual[listPosition - 1]);
     }
 
-    
+    [Theory]
+    [InlineData(1, 5, 5)]
+    [InlineData(-2, 5, 8)]
+    [InlineData(-10, -4, 7)]
+    public void GetAllTokens_CustomSequenceConstructor_ReturnsTheCorrectLength(int startNumber, int finalNumber, int expectedResultLength)
+    {
+        var actual = new GenerateAllFizzBuzzTokens(startNumber, finalNumber).GetAllTokens();
+
+        Assert.Equal(expectedResultLength, actual.Count);
+    }
+
+
 }
