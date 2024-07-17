3a69dedc732147339e82105db7f509a65d9c51a3
Richardson Rosa <richardsonwtr@gmail.com>
2024-07-16 21:10:38 -0300

    refactor(data-generation): speeding up creation by adding transactions.

--- HardChallenge/SmartVault.DataGeneration/GenerateData.cs
+++ HardChallenge/SmartVault.DataGeneration/GenerateData.cs
@@ -14,36 +14,43 @@ namespace SmartVault.DataGeneration
         public void Generate(string databaseConnectionString, int numberOfUsers, int numberOfDocumentsPerUser, string fileName) {
             using (var connection = new SQLiteConnection(databaseConnectionString))
             {
-                var files = Directory.GetFiles(@"..\..\..\..\BusinessObjectSchema");
-                for (int i = 0; i <= 2; i++)
+                connection.Open();
+                using (var transaction = connection.BeginTransaction())
                 {
-                    var serializer = new XmlSerializer(typeof(BusinessObject));
-                    var businessObject = serializer.Deserialize(new StreamReader(files[i])) as BusinessObject;
-                    connection.Execute(businessObject?.Script);
-
-                }
-                var documentNumber = 0;
-                for (int i = 0; i < numberOfUsers; i++)
-                {
-                    var randomDayIterator = RandomDay().GetEnumerator();
-                    randomDayIterator.MoveNext();
-                    connection.Execute($"INSERT INTO User (Id, FirstName, LastName, DateOfBirth, AccountId, Username, Password) VALUES('{i}','FName{i}','LName{i}','{randomDayIterator.Current.ToString("yyyy-MM-dd")}','{i}','UserName-{i}','e10adc3949ba59abbe56e057f20f883e')");
-                    connection.Execute($"INSERT INTO Account (Id, Name) VALUES('{i}','Account{i}')");
-
-                    for (int d = 0; d < numberOfDocumentsPerUser; d++, documentNumber++)
+                    var files = Directory.GetFiles(@"..\..\..\..\BusinessObjectSchema");
+                    for (int i = 0; i <= 2; i++)
                     {
-                        var documentPath = new FileInfo(fileName).FullName;
-                        connection.Execute($"INSERT INTO Document (Id, Name, FilePath, Length, AccountId) VALUES('{documentNumber}','Document{i}-{d}.txt','{documentPath}','{new FileInfo(documentPath).Length}','{i}')");
-                    }
-                }
+                        var serializer = new XmlSerializer(typeof(BusinessObject));
+                        var businessObject = serializer.Deserialize(new StreamReader(files[i])) as BusinessObject;
+                        connection.Execute(businessObject?.Script);
 
-                var accountData = connection.Query("SELECT COUNT(*) FROM Account;");
-                Console.WriteLine($"AccountCount: {JsonConvert.SerializeObject(accountData)}");
-                var documentData = connection.Query("SELECT COUNT(*) FROM Document;");
-                Console.WriteLine($"DocumentCount: {JsonConvert.SerializeObject(documentData)}");
-                var userData = connection.Query("SELECT COUNT(*) FROM User;");
-                Console.WriteLine($"UserCount: {JsonConvert.SerializeObject(userData)}");
+                    }
+                    var documentNumber = 0;
+                    for (int i = 0; i < numberOfUsers; i++)
+                    {
+                        var randomDayIterator = RandomDay().GetEnumerator();
+                        randomDayIterator.MoveNext();
+                        connection.Execute($"INSERT INTO User (Id, FirstName, LastName, DateOfBirth, AccountId, Username, Password) VALUES('{i}','FName{i}','LName{i}','{randomDayIterator.Current.ToString("yyyy-MM-dd")}','{i}','UserName-{i}','e10adc3949ba59abbe56e057f20f883e')");
+                        connection.Execute($"INSERT INTO Account (Id, Name) VALUES('{i}','Account{i}')");
+
+                        for (int d = 0; d < numberOfDocumentsPerUser; d++, documentNumber++)
+                        {
+                            var documentPath = new FileInfo(fileName).FullName;
+                            connection.Execute($"INSERT INTO Document (Id, Name, FilePath, Length, AccountId) VALUES('{documentNumber}','Document{i}-{d}.txt','{documentPath}','{new FileInfo(documentPath).Length}','{i}')");
+                        }
+                    }
+
+                    var accountData = connection.Query("SELECT COUNT(*) FROM Account;");
+                    Console.WriteLine($"AccountCount: {JsonConvert.SerializeObject(accountData)}");
+                    var documentData = connection.Query("SELECT COUNT(*) FROM Document;");
+                    Console.WriteLine($"DocumentCount: {JsonConvert.SerializeObject(documentData)}");
+                    var userData = connection.Query("SELECT COUNT(*) FROM User;");
+                    Console.WriteLine($"UserCount: {JsonConvert.SerializeObject(userData)}");
+
+                    transaction.Commit();
+                }
             }
+            
         }
 
         static IEnumerable<DateTime> RandomDay()
