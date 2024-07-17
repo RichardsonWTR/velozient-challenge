bd25a45bce0d3b7232d44ee99223023f7093ec83
Richardson Rosa <richardsonwtr@gmail.com>
2024-07-17 01:59:35 -0300

    feat(program): saving text from users if contains 'Smith Property' in a new file.

--- HardChallenge/SmartVault.Program/Program.cs
+++ HardChallenge/SmartVault.Program/Program.cs
@@ -1,15 +1,23 @@
-﻿namespace SmartVault.Program
+﻿using FileUtils;
+using Microsoft.Extensions.Configuration;
+using SmartVault.DataGeneration;
+using SmartVault.Program.BusinessObjects;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace SmartVault.Program
 {
     partial class Program
     {
         static void Main(string[] args)
         {
-            if (args.Length == 0)
+            if (args.Length < 2)
             {
                 return;
             }
 
-            WriteEveryThirdFileToFile(args[0]);
+            WriteEveryThirdFileToFile(args[0], args[1]);
             GetAllFileSizes();
         }
 
@@ -18,9 +26,23 @@
             // TODO: Implement functionality
         }
 
-        private static void WriteEveryThirdFileToFile(string accountId)
+
+        // FIXME: build database path dynamically, remove it from the cli args.
+        private static void WriteEveryThirdFileToFile(string accountId, string databasePath, string outputFilePath = "output-file.txt")
         {
-            // TODO: Implement functionality
+            var configuration = new GetConfig().config;
+
+            var databaseConnectionString = string.Format(configuration?["ConnectionStrings:DefaultConnection"] ?? "", databasePath);
+
+            List<string> recordFilePaths = new FilterNthFileFromUser(int.Parse(accountId), 3).FilterDocumentRowPaths(databaseConnectionString);
+
+            foreach (var recordFilePath in recordFilePaths) {
+                if (!(new CheckIfFileHasText(recordFilePath).HasText("Smith Property")))
+                    continue;
+
+                string content = File.ReadAllText(recordFilePath);
+                File.AppendAllText(outputFilePath, content);
+            }
         }
     }
 }
