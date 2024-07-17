763224cad84b6190e68b6418438553d3316cfe21
Richardson Rosa <richardsonwtr@gmail.com>
2024-07-17 00:32:29 -0300

    feat(file-utils): checking if file has text

---
+++ HardChallenge/FileUtils.Tests/CheckIfFileHasTextTests.cs
@@ -0,0 +1,48 @@
+﻿
+namespace FileUtils.Tests
+{
+    public class CheckIfFileHasTextTests
+    {
+        [Theory]
+        [InlineData("some")]
+        [InlineData("content")]
+        [InlineData("me")]
+        [InlineData("me con")]
+        public void HasText_ReturnsTrueIfHasText(string textToSearch)
+        {
+            string fileName = Path.GetTempPath() + Guid.NewGuid().ToString() + ".txt";
+            File.WriteAllText(fileName, "some content");
+
+            var result = new CheckIfFileHasText(fileName).HasText(textToSearch);
+
+            Assert.True(result);
+        }
+
+        [Theory]
+        [InlineData("another")]
+        [InlineData("--content")]
+        public void HasText_ReturnsFalseIfDoesNotHaveText(string textToSearch)
+        {
+            string fileName = Path.GetTempPath() + Guid.NewGuid().ToString() + ".txt";
+            File.WriteAllText(fileName, "some content");
+
+            var result = new CheckIfFileHasText(fileName).HasText(textToSearch);
+
+            Assert.False(result);
+        }
+
+
+        [Theory(Skip = "To be implemented")]
+        [InlineData("some content")]
+        [InlineData("some")]
+        public void HasText_ReturnsTrueIfHasTextSplittedIntoMultipleLines(string textToSearch)
+        {
+            string fileName = Path.GetTempPath() + Guid.NewGuid().ToString() + ".txt";
+            File.WriteAllText(fileName, "so\nme\ncon\ntent");
+
+            var result = new CheckIfFileHasText(fileName).HasText(textToSearch);
+
+            Assert.True(result);
+        }
+    }
+}
---
+++ HardChallenge/FileUtils/CheckIfFileHasText.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileUtils
+{
+    public class CheckIfFileHasText
+    {
+        private readonly string _filename;
+
+        public CheckIfFileHasText(string fileName)
+        {
+            _filename = fileName;
+        }
+
+        public bool HasText(string textToSearch)
+        {
+            var output = File.ReadLines(_filename).
+                Where(line => line.Contains(textToSearch)).
+                FirstOrDefault();
+            return output != null ? output.Any() : false;
+        }
+    }
+}
